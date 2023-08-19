969a59d5cd3ad29463dc210be9553e4f9834f7d9
bettayub <bettayub19@gmail.com>
2023-08-19 18:51:55 +0300

    ading a procedure for  test cases

--- Numbers.py
+++ Numbers.py
@@ -1,10 +1,16 @@
-def exactly_two_positive(a, b, c):
+def two_positive_numbers(a, b, c):
     positives = [num for num in (a, b, c) if num > 0]
     return len(positives) == 2
 
-print(exactly_two_positive(2, 4, -3))  # True
-print(exactly_two_positive(-4, 6, 8))  # True
-print(exactly_two_positive(4, -6, 9))  # True
-print(exactly_two_positive(-4, 6, 0))  # False
-print(exactly_two_positive(4, 6, 10))  # False
-print(exactly_two_positive(-14, -3, -4))  # False
+
+#test cases for this code
+print(two_positive_numbers(2, 4, -3))  # True
+print(two_positive_numbers(-4, 6, 8))  # True
+print(two_positive_numbers(4, -6, 9))  # True
+print(two_positive_numbers(-4, 6, 0))  # False
+print(two_positive_numbers(4, 6, 10))  # False
+print(two_positive_numbers(-14, -3, -4))  # False
+
+#how to run
+#python3 Numbers.py
+
