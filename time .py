1d7ec8e8b4a2a52d009354c4511376c842b25304
bettayub <bettayub19@gmail.com>
2023-08-19 15:00:12 +0300

    using if condition statement to return a true statement when hour is equal to 12 hour format

--- time .py
+++ time .py
@@ -2,3 +2,10 @@ def my_time_to_24hr(hour, minute, period):
     if period == "am":
         if hour == 12:
             hour_24 = 0
+        else:
+            hour_24 = hour
+    else:  # period == "pm"
+        if hour == 12:
+            hour_24 = 12
+        else:
+            hour_24 = hour + 12
