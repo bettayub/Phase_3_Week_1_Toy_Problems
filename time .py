9e1015eebc46ba71fffe0f49e80a5474b9e43e3f
bettayub <bettayub19@gmail.com>
2023-08-19 15:08:59 +0300

    Add test cases for time conversion function

--- time .py
+++ time .py
@@ -11,3 +11,12 @@ def my_time_to_24hr(hour, minute, period):
             hour_24 = hour + 12
     
     return f"{hour_24:02d}{minute:02d}"
+
+
+# List of test cases: (hour, minute, period)
+test_cases = [
+    (8, 30, "pm"),
+    (1, 30, "am"),
+    (12, 15, "am"),
+    (12, 00, "m")
+]
