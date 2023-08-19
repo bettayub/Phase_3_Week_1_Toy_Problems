dcbd3e851f86bed21b7c5a03df900409f1677ae0
bettayub <bettayub19@gmail.com>
2023-08-20 00:26:20 +0300

    Loop through input string to calculate consonant substring values

--- Constant_value.py
+++ Constant_value.py
@@ -2,6 +2,14 @@ def solution(input_string):
     consonant_values = {'b': 2, 'c': 3, 'd': 4, 'f': 6, 'g': 7, 'h': 8, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
                         'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'v': 21, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
     
-     current_value = 0
+    current_value = 0
     max_value = 0
-  
+    
+    for character in input_string:
+        if character in consonant_values:
+            current_value += consonant_values[character]
+        else:
+            max_value = max(max_value, current_value)
+            current_value = 0
+    
+    max_value = max(max_value, current_value)
