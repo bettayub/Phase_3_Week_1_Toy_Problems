e24b2d90383d601478d3274bb6b7f503815569ac
bettayub <bettayub19@gmail.com>
2023-08-20 00:20:57 +0300

    assigning a variable consonant_values  key and value for mapping characters

--- Constant_value.py
+++ Constant_value.py
@@ -1,2 +1,5 @@
-def solve(input_string):
+def solution(input_string):
+    consonant_values = {'b': 2, 'c': 3, 'd': 4, 'f': 6, 'g': 7, 'h': 8, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
+                        'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'v': 21, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+    
   
